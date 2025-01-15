a6f5b782b5f7d1b3240b281e24b9286296b4f646
Ketan Shinde <ketan_shinde@hotmail.com>
2025-01-15 22:47:07 +0530

    npm multer is installed

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const path= require("path");
 const express = require("express");
+const multer = require("multer");
+
 
 const app = express();
-const Port= 8000;
+const Port= 8002;
+const storage = multer.diskStorage({
+    destination:function(req, file, cb){
+        return cb(null, "/uploads")
+    },
+    filename:function(req, file, cb){
+        return cb(null, `${Date.now()}-${file.originalname}`)
+    },
+}) 
+const uploads = multer({storage:storage})
+
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.use(express.json());
+app.use(express.urlencoded({extended:'false'}));
 
 app.get("/", (req, res)=>{
     return res.render("homepage");
 });
 
-app.listen(Port,()=>{`server started on Port: ${Port}`});
+app.post('/upload',uploads.single('profileImage'), (req, res)=>{
+    console.log(req.body);
+    console.log(req.file);
+    res.redirect("/");
+});
+
+app.listen(Port,()=>{console.log(`server started on Port: ${Port}`)});
