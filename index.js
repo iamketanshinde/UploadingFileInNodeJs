419509934c3bdbe669d2791921e736ae86d1f67e
Ketan Shinde <ketan_shinde@hotmail.com>
2025-01-15 21:55:08 +0530

    index.js & homepage file created

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('First Commit!')
+const path= require("path");
+const express = require("express");
+
+const app = express();
+const Port= 8000;
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    return res.render("homepage");
+});
+
+app.listen(Port,()=>{`server started on Port: ${Port}`});
